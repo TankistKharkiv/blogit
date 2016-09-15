8f865c5744cca879e19450fb4c480d81bc2566b6
tankistkharkiv <tankistkharkiv@gmail.com>
2016-09-16 00:34:22 +0300

    fc fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Blogit::Engine.routes.draw do
   # Keep these above the posts resources block
   get "posts/page/:page" => "posts#index"
   get "posts/tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
+  get "categories/:category" => 'categories#show'
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
