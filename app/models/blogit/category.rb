eff9447c818aa9e4abeb9aeea951f9b4c59c94a8
tankistkharkiv <tankistkharkiv@gmail.com>
2016-09-15 21:58:04 +0300

    fc fix

--- app/models/blogit/category.rb
+++ app/models/blogit/category.rb
@@ -1,5 +1,5 @@
 module Blogit
   class Category < ActiveRecord::Base
-    has_many :posts, :class_name => 'Blogit::Post'
+    has_many :posts, :class_name => 'Blogit::Post', foreign_key: 'post_id'
   end
 end
