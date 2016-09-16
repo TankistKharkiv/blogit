c8b7aa816f6578ce52d928ef7df09eeb7416f253
dmitry.maltsev <dmitry.maltsev@roobykon.com>
2016-09-16 10:25:33 +0300

    add categories model

--- db/migrate/20160915171800_create_blogit_categories.rb
+++ db/migrate/20160915171800_create_blogit_categories.rb
@@ -2,6 +2,7 @@ class CreateBlogitCategories < ActiveRecord::Migration
   def change
     create_table :blogit_categories do |t|
       t.string :name
+      t.timestamps
     end
     add_reference :blogit_posts, :category
     add_index :blogit_posts, :category_id
