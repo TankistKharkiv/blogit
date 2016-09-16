eb67f1f86a561a3f637bbcf730f73b77c32b8046
dmitry.maltsev <dmitry.maltsev@roobykon.com>
2016-09-16 16:33:46 +0300

    add tags

--- app/controllers/blogit/categories_controller.rb
+++ app/controllers/blogit/categories_controller.rb
@@ -5,7 +5,8 @@ module Blogit
     layout Blogit.configuration.layout if Blogit.configuration.layout
 
     def show
-      @total_posts_count = Category.find(params[:category]).posts.length
+      posts_for_category =Category.find(params[:category]).posts
+      @total_posts_count = posts_for_category.length
       @posts = posts_for_category.for_index page_number
       @tags = posts_for_category.tag_counts_on(:tags)
       yield post if block_given?
