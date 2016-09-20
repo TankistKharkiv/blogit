e4bc4913cd0026339b70947d71e77ccad5e965fd
dmitry.maltsev <dmitry.maltsev@roobykon.com>
2016-09-20 15:41:48 +0300

    add tags

--- app/models/blogit/category.rb
+++ app/models/blogit/category.rb
@@ -2,7 +2,7 @@ module Blogit
   class Category < ActiveRecord::Base
     has_many :posts, :class_name => 'Blogit::Post'
     def to_param
-      "#{name.parameterize}"
+      "#{id}-#{name.parameterize}"
     end
   end
 end
